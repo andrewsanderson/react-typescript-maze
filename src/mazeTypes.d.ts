4ca0976c288e6ac4dfe25953313946729fed7fb5
andrewsanderson <andrewpetersanderson@gmail.com>
2022-09-05 21:02:33 +1000

    maze utilities expansion

--- src/mazeTypes.d.ts
+++ src/mazeTypes.d.ts
@@ -2,3 +2,12 @@ interface MazeSettings {
   height: number;
   width: number;
 }
+
+type Coordinates = [number, number];
+
+interface Cell {
+  coordinates: Coordinates;
+  walls: [boolean, boolean, boolean, boolean];
+}
+
+type Maze = Array<Cell>;
