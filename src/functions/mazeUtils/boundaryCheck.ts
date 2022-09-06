7eb3efc03d7ce2fd2a74a08d1efd9ffa1872e9da
andrewsanderson <andrewpetersanderson@gmail.com>
2022-09-06 10:54:56 +1000

    Update boundaryCheck.ts

--- src/functions/mazeUtils/boundaryCheck.ts
+++ src/functions/mazeUtils/boundaryCheck.ts
@@ -2,7 +2,9 @@ const boundaryCheck = (
   coordinates: Coordinates,
   mazeSettings: MazeSettings
 ): boolean => {
-  return true;
+  const [x, y] = coordinates;
+  const { height, width } = mazeSettings;
+  return x >= 0 && x < width && y >= 0 && y < height;
 };
 
 export default boundaryCheck;
