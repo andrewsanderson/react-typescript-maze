df22883d65421001582cd2588dbc2c50a3dc6721
andrewsanderson <andrewpetersanderson@gmail.com>
2022-09-26 09:31:22 +1000

    Update loop.d.ts

--- src/functions/pathing/loop/loop.d.ts
+++ src/functions/pathing/loop/loop.d.ts
@@ -1,15 +1,11 @@
 /** Provided the maze and a path, this function will return true if the current last node meets the win criteria. */
 type WinConditions = (path: Path) => boolean;
 
-/** Required for iterative functions to hault to halt their functionality (likely if the number of exhausted nodes is equal to the number of nodes in the maze). */
-type LoseConditions = (path: Path) => boolean;
-
 /** Returns the first node in the maze that meets the starting criteria. */
 type StartingCriteria = (maze: Maze) => Cell;
 
 /** Combines the above criteria to loop using a traversal function. */
 type LoopMethod = (
   startingCriteria: StartingCriteria,
-  winConditions: WinConditions,
-  loseConditions?: LoseConditions
+  winConditions: WinConditions
 ) => (maze: Maze, traversaleAlgorithm: TraversaleAlgorithm) => Nodes;
